e95b6bc6e1c1e40520091873e8548c9ea94a039c
vince <vincent@djengo.net>
2012-08-16 15:58:48 +0200

    déplacement de tolk_engine.rb

---
+++ tolk_engine.rb
@@ -0,0 +1,26 @@
+require "lib/tolk_engine/engine"
+require 'ya2yaml'
+
+module TolkEngine
+  # Our host application root path
+  # We set this when the engine is initialized
+   mattr_accessor :app_root
+
+    # Yield self on setup for nice config blocks
+  def self.setup
+    yield self
+  end
+
+  # Setup TolkEngine
+  def self.config(&block)
+    if block_given?
+      block.call(TolkEngine::Config)
+    else
+      TolkEngine::Config
+    end
+  end
+end
+
+
+# Require our engine
+require "lib/tolk_engine/engine"
